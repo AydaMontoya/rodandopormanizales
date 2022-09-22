03145fe6a1172433c7bdbf270dabd99ca2d7e65f
Ayda Yineth Montoya Henao <aidamontoya96@gmail.com>
2022-09-21 23:05:32 -0500

    Se actualiza información en  los componentes de dificultad

--- src/views/DifAvanzadaView.vue
+++ src/views/DifAvanzadaView.vue
@@ -1,14 +1,60 @@
 <template>
-  <componentDificultad/>
+  <div class="titulo">
+    <h1>Escoge tu aventura !!!</h1>
+  </div>
+  <div class="dificultades">
+    <componentDificultad
+      image="/assets/Letras.jpg"
+      ruta="Alto de letras"
+      distancia="71.21 km"
+      desnivel="1.894 m"
+      tipodebicicleta="MTB-RUTA"
+    />
+    <componentDificultad
+      image="/assets/Chinchina.jpg"
+      ruta="Chinchina"
+      distancia="45.42 km"
+      desnivel="1.056 m"
+      tipodebicicleta="MTB"
+    />
+    <componentDificultad
+      image="/assets/Neira.jpg"
+      ruta="Neira"
+      distancia="41.44 km"
+      desnivel="1.116 m"
+      tipodebicicleta="MTB"
+    />
+  </div>
 </template>
 
 <script>
-import componentDificultad from '@/components/ComponentDificultad.vue'
+import componentDificultad from "@/components/ComponentDificultad.vue";
 
 export default {
- name: 'HomeView',
- components: {
-   componentDificultad
-},
-}
+  name: "HomeView",
+  components: {
+    componentDificultad,
+  },
+};
 </script>
+
+<style>
+.titulo {
+  color: #0c233e;
+  text-align: center;
+}
+
+.dificultades {
+  background: #f2f2f2;
+  width: 90%;
+  margin: 0 auto;
+  padding: 100px 0px 0px 0px;
+  max-width: 1200px;
+  min-height: 100vh;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  gap: 4.2em;
+  flex-wrap: wrap;
+}
+</style>
--- src/views/DifIntermediaView.vue
+++ src/views/DifIntermediaView.vue
@@ -1,5 +1,60 @@
 <template>
-  <div class="ciclismoColombiano">
-    <h1>This is an about page</h1>
+  <div class="titulo">
+    <h1>Escoge tu aventura !!!</h1>
+  </div>
+  <div class="dificultades">
+    <componentDificultad
+      image="/assets/Acuaparque.jpg"
+      ruta="Acuaparque"
+      distancia="29.68 km"
+      desnivel="723 m"
+      tipodebicicleta="MTB-RUTA"
+    />
+    <componentDificultad
+      image="/assets/La puerta.jpg"
+      ruta="La puerta"
+      distancia="18.98 km"
+      desnivel="451 m"
+      tipodebicicleta="MTB"
+    />
+    <componentDificultad
+      image="/assets/ReservadelRuiz.jpg"
+      ruta="Reserva del ruiz"
+      distancia="34.38 km"
+      desnivel="794 m"
+      tipodebicicleta="MTB"
+    />
   </div>
 </template>
+
+<script>
+import componentDificultad from "@/components/ComponentDificultad.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    componentDificultad,
+  },
+};
+</script>
+
+<style>
+.titulo {
+  color: #0c233e;
+  text-align: center;
+}
+
+.dificultades {
+  background: #f2f2f2;
+  width: 90%;
+  margin: 0 auto;
+  padding: 100px 0px 0px 0px;
+  max-width: 1200px;
+  min-height: 100vh;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  gap: 4.2em;
+  flex-wrap: wrap;
+}
+</style>
