83d6c42d174a53cbeb4e904eb4f3333f8a5581cb
Ayda Yineth Montoya Henao <aidamontoya96@gmail.com>
2022-10-01 00:56:49 -0500

    Se modifica componente ApiWeather

--- src/components/organisms/ApiWeather.vue
+++ src/components/organisms/ApiWeather.vue
@@ -1,37 +1,55 @@
 <template>
-    <div class="video-container">
-      <div class="video-item video-1">
-        <h3 class="subtitle">Revisa el clima antes de salir a Rodar</h3>
-        <hr />
-        <p>
-          Trae los datos desde la API
-        </p>
-        <button v-on:click="mounted"> --> Click aquí para traer
-           información</button>
-        <div id="app">
-          {{ info }}
-        </div>
+  <div class="video-container">
+    <div class="video-item video-1">
+      <h3 class="subtitle">Revisa el clima antes de salir a Rodar</h3>
+      <hr />
+      <p>Trae los datos desde la API</p>
+      <button v-on:click="weather">
+        --> Click aquí para traer información
+      </button>
+      <div id="app">
+        {{ clima }}
       </div>
+      <p>{{temperatura_maxima}} temperatura</p>
+      <h1>{{ciudad}}</h1>
     </div>
-  </template>
+  </div>
+</template>
 
 <script>
-    import axios from 'axios';
-    
-    export default {
-      name: 'App',
-      data() {
-        return {
-          info: null,
-        };
-      },
-      methods: {
-        mounted() {
-          axios
-            .get('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5f3e2ffb975979235a637c386a62fb9a')
-            .then((response) => { this.info = response.data.weather });
-          console.log(this.info[0])
-          }
-      },
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      info: null,
+      clima: null,
+      temperatura_maxima:null,
+      ciudad:null
     };
-    </script>
+  },
+  methods: {
+    weather() {
+      axios
+        .get(
+          "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5f3e2ffb975979235a637c386a62fb9a"
+        )
+        .then((response) => {
+          this.info = response.data.weather;
+          this.clima = this.info[0].description;
+          this.temperatura_maxima = response.data.main.temp_max;
+          this.ciudad=response.data.name;
+        })
+        ;
+
+    },
+  },
+};
+</script>
+
+<style scoped>
+#app{
+  color:red;
+}
+</style>
